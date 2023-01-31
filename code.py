d3b16f104e8aefb6148da2385a1ef88efec4bba6
Assel Amanzholova <amanzholova-2003@inbox.ru>
2023-01-31 09:11:50 +0600

    add fibonacci function

--- code.py
+++ code.py
@@ -1,3 +1,18 @@
 # Amanzholova Assel lab 2
 print("Amanzholova Assel lab 2")
 
+
+
+def fibonacci(n):
+	if (n == 0):
+		return 0
+	elif (n == 1):
+		return 1
+	else:
+		return fibonacci(n - 1) + fibonacci(n - 2)
+		
+
+n = 8
+fn = 0
+fn = fibonacci(n)
+print('n = ', fn)
