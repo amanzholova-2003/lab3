f5d3e28604109eb56f60844f655b1440470dd3af
Assel Amanzholova <amanzholova-2003@inbox.ru>
2023-02-07 09:41:11 +0600

    lab3.4.py commit

--- lab3.4.py
+++ lab3.4.py
@@ -1,6 +1,5 @@
 n = int(input())
-sum = 0
-for i in range(1, n + 1):
-    sum += i
-sum == n * (n + 1) // 2
-print(sum)
+su =int( (n+1)*n/2)
+for i in range (1,n ):
+    su -= int(input())
+print(su)
